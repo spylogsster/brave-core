4b9806a07707d90e2ec6e0e52895975a35b6e2ec
Claudio DeSouza <cdesouza@brave.com>
2023-07-28 07:56:36 +0100

    Corrrect to `vector<raw_ptr<T>>` upstream use
    Chromium has been gradually phasing out naked pointers, and replacing it
    with instrumentation that allows dangling pointer detection. This time,
    all vectors storing pointer types have been changed to store `raw_ptr`
    instances, annotated with dangling tolerance.
    
    This change corrects brave code to reflect the new configuration of
    upstream code. In some cases, the removal of `auto` is enough to force a
    conversion from the `raw_ptr` type, to the underlying pointer, and
    that's what was nearly everywhere. In other cases, our own vector type
    spelling had to be changed to match what was being done upstream.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/8164060989632ef27c0e86809572183f55f02e83
    
    commit 8164060989632ef27c0e86809572183f55f02e83 (tag: 117.0.5911.3)
    Author: Ali Hijazi <ahijazi@chromium.org>
    Date:   Thu Jul 27 00:51:06 2023 +0000
    
        Rewrite vector<T*> into vector<raw_ptr<T>>
    
        First Patch:
           1- Introducing alias dangling_raw_ptr<T> =
                raw_ptr<T,DanglingUntriaged>
           2- Annotate Some fields with RAW_PTR_EXCLUSION
           3- Introduce some needed functions
           4- Preliminary fixes
           5- Set VectorRawPtrExperiment synthetic field trial to enabled
    
        Second patch: Automatic rewrite
        Later patches: Manual fixes
    
        Bug: 1432993

--- chromium_src/chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.cc
+++ chromium_src/chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.cc
@@ -109,7 +109,8 @@ void DontAskAgainCheckbox::ButtonPressed() {
 }
 
 bool HasWidevinePermissionRequest(
-    const std::vector<permissions::PermissionRequest*>& requests) {
+    const std::vector<dangling_raw_ptr<permissions::PermissionRequest>>&
+        requests) {
   // When widevine permission is requested, |requests| only includes Widevine
   // permission because it is not a candidate for grouping.
   if (requests.size() == 1 &&
@@ -122,7 +123,8 @@ bool HasWidevinePermissionRequest(
 
 void AddAdditionalWidevineViewControlsIfNeeded(
     views::BubbleDialogDelegateView* dialog_delegate_view,
-    const std::vector<permissions::PermissionRequest*>& requests) {
+    const std::vector<dangling_raw_ptr<permissions::PermissionRequest>>&
+        requests) {
   if (!HasWidevinePermissionRequest(requests)) {
     return;
   }
@@ -164,7 +166,8 @@ void AddWidevineFootnoteView(
 #else
 void AddAdditionalWidevineViewControlsIfNeeded(
     views::BubbleDialogDelegateView* dialog_delegate_view,
-    const std::vector<permissions::PermissionRequest*>& requests) {}
+    const std::vector<dangling_raw_ptr<permissions::PermissionRequest>>&
+        requests) {}
 #endif
 
 // Custom combobox, shows permission lifetime options and applies selected value
@@ -241,7 +244,8 @@ views::View* AddPermissionLifetimeComboboxIfNeeded(
 
 void AddFootnoteViewIfNeeded(
     views::BubbleDialogDelegateView* dialog_delegate_view,
-    const std::vector<permissions::PermissionRequest*>& requests,
+    const std::vector<dangling_raw_ptr<permissions::PermissionRequest>>&
+        requests,
     Browser* browser) {
 #if BUILDFLAG(ENABLE_WIDEVINE)
   // Widevine permission bubble has custom footnote.
