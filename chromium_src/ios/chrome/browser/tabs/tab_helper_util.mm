9d931bcd5f300b5984b6cd6c2ba4798c3aaca413
Claudio DeSouza <cdesouza@brave.com>
2023-09-19 15:46:30 +0300

    [ios] `complex_tasks` with `model` target
    This change corrects the inclusion path for one of the headers that was
    moved under `complex_tasks/model`.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/d68ad558a83f42328da41ea5a4b616a9c1afe585
    
    commit d68ad558a83f42328da41ea5a4b616a9c1afe585
    Author: Aman Verma <amanvr@google.com>
    Date:   Fri Sep 15 20:57:41 2023 +0000
    
        [iOS] Move ios/c/b/complex_tasks to model subfolder
    
        To simplify the folder hierarchy on iOS and have a clearer layer
        separation, all the files in ios/c/b/<foo>/ will be moved to
        ios/c/b/<foo>/model/ to showcase that they are model files.
    
        Bug: 1319852, 1480285

--- chromium_src/ios/chrome/browser/tabs/tab_helper_util.mm
+++ chromium_src/ios/chrome/browser/tabs/tab_helper_util.mm
@@ -1,10 +1,11 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/* Copyright (c) 2017 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #import "ios/chrome/browser/tabs/tab_helper_util.h"
 
-#include "ios/chrome/browser/complex_tasks/ios_task_tab_helper.h"
+#include "ios/chrome/browser/complex_tasks/model/ios_task_tab_helper.h"
 #include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
 #include "ios/chrome/browser/sync/ios_chrome_synced_tab_delegate.h"
 #include "ios/chrome/browser/tabs/tab_helper_util.h"
