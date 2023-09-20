fe0fa0128976f35cc5780a2141c505756f1ffc86
mkarolin <max@brave.com>
2023-09-20 10:44:25 -0400

    [check_chromium_src] Updated how #defines and #includes are checked.
    -Do not skip #defines that have no replacement values.
    -Expect #define to be at the beginning of the line.
    -Check for corresponding #undef.
    -Check for #defines that are used internally in the override and ignore
     those.
    -Always check #includes.
    -v8/src #includes can start wit src.
    -Return success/error code.
    -Strip comments from sources and overrides to avoid finding symbols in comments.

--- script/check_chromium_src.py
+++ script/check_chromium_src.py
@@ -31,8 +31,8 @@ BRAVE_CHROMIUM_SRC = os.path.join(BRAVE_SRC, 'chromium_src')
 CHROMIUM_SRC = os.path.abspath(os.path.dirname(BRAVE_SRC))
 
 # pylint: disable=line-too-long
-NORMAL_DEFINITIONS_REGEXP = r'#define[\s\\]+([a-zA-Z0-9_]+[^\s\(]*)(?:[ \t]+\\\s*|[ \t])+([a-zA-Z0-9_]+[^\s\(]*)'
-FUNCTION_LIKE_DEFINITIONS_REGEXP = r'#define[\s\\]+([a-zA-Z0-9_]+)[\s\\]*\(.*?\)(?:[ \t]+\\\s*|[ \t])([a-zA-Z0-9_]*[\s\\]*\(.*?\))'
+NORMAL_DEFINITIONS_REGEXP = r'^#define[\s\\]+([a-zA-Z0-9_]+[^\s\(]*)(?:[ \t]+\\\s*|[ \t])?([a-zA-Z0-9_]+[^\s\(]*)?$'
+FUNCTION_LIKE_DEFINITIONS_REGEXP = r'^#define[\s\\]+([a-zA-Z0-9_]+)[\s\\]*\(.*?\)(?:[ \t]+\\\s*|[ \t])?([a-zA-Z0-9_]*[\s\\]*\(.*?\))?'
 # pylint: enable=line-too-long
 
 RE_EXCLUDES = [
@@ -89,19 +89,14 @@ GRIT_INCLUDES = []
 
 def do_check_includes(override_filepath):
     """
-    Warns if |override_filepath| uses relative includes and, if so, also checks
-    them for the correct number of ".." in the include statement for the
-    original file.
+    Errors if |override_filepath| uses relative includes and also checks
+    src/ and ../gen-prefixed includes for naming consistency between the
+    original and the override.
     """
+    error_count = 0
     with open(override_filepath, mode='r', encoding='utf-8') as override_file:
         normalized_override_filepath = override_filepath.replace('\\', '/')
         override_filename = os.path.basename(override_filepath)
-        override_dirpath = os.path.dirname(override_filepath)
-
-        # Calculate the expected number of parent dirs.
-        # -1 for the dir where the file is.
-        # +3 to get to chromium_src->brave->src.
-        expected_count = len(override_dirpath.split(os.path.sep)) - 1 + 3
 
         for line in override_file:
             # Check src/-prefixed includes
@@ -109,57 +104,107 @@ def do_check_includes(override_filepath):
             line_match = re.search(regexp, line)
             if line_match:
                 if line_match.group(1) != normalized_override_filepath:
-                    print(f"WARNING: {override_filepath} uses a src/-prefixed" +
+                    # Check for v8 overrides, they can have includes starting
+                    # with src.
+                    if normalized_override_filepath.startswith("v8/src"):
+                        continue
+                    print(f"ERROR: {override_filepath} uses a src/-prefixed" +
                           " include that doesn't point to the expected file:")
-                    print(f"         Include: {line}" +
-                          "         Expected include target: src/" +
+                    print(f"       Include: {line}" +
+                          "       Expected include target: src/" +
                           f"{normalized_override_filepath}")
                     print("-------------------------")
+                    error_count += 1
                 continue
             gen_regexp = r'^#include "../gen/(.*)"'
             line_match = re.search(gen_regexp, line)
             if line_match:
                 if line_match.group(1) != normalized_override_filepath:
-                    print(f"WARNING: {override_filepath} uses a ../gen/" +
+                    print(f"ERROR: {override_filepath} uses a ../gen/" +
                           "-prefixed include that doesn't point to the " +
                           "expected file:")
-                    print(f"         Include: {line}" +
-                          "         Expected include target: ../gen/" +
+                    print(f"       Include: {line}" +
+                          "       Expected include target: ../gen/" +
                           f"{normalized_override_filepath}")
                     print("-------------------------")
+                    error_count += 1
                 continue
 
-            # Check relative includes go up the expected amount of steps.
-            # We're only interested in relative include paths.
+            # Check for relative includes.
             regexp = rf'^#include "(\.\./.*{override_filename})"'
             line_match = re.search(regexp, line)
             if not line_match:
                 continue
 
-            print(f"WARNING: {override_filepath} uses a relative include:\n" +
-                  f"         {line}" +
-                  "         Switch to using a src/-prefixed include instead.")
+            print(f"ERROR: {override_filepath} uses a relative include:\n" +
+                  f"       {line}" +
+                  "       Switch to using a src/-prefixed include instead.")
             print("-------------------------")
+            error_count += 1
+    return error_count
 
-            # Count the number of '../' elements, but don't use the OS's path
-            # separator here, since we're counting paths from a C++ include.
-            include_path = line_match.group(1)
-            actual_count = include_path.split('/').count('..')
 
-            # Check actual vs expected.
-            if actual_count != expected_count:
-                print(f"ERROR: while processing {override_filepath}")
+def strip_comments(content):
+    """
+    Strips comments from c++ file content
+    """
+    MULTILINE_COMMENT_REGEX=r'(/\*([\s\S]*?)\*/)'
+    ONE_LINE_COMMENT_REGEX=r'(\s*//.*$)'
+    stripped = re.sub(MULTILINE_COMMENT_REGEX, '', content, flags=re.MULTILINE)
+    return re.sub(ONE_LINE_COMMENT_REGEX, '', stripped, flags=re.MULTILINE)
 
-                # Sanity check
-                saved_cwd = os.getcwd()
-                os.chdir(override_dirpath)
-                if not os.path.isfile(include_path):
-                    print(f"       File {include_path} doesn't exist")
-                os.chdir(saved_cwd)
 
-                print(f"       Expected {expected_count} \"..\"")
-                print(f"       Found {actual_count} \"..\"")
-                print("-------------------------")
+def validate_define(override_filepath, content, target):
+    """
+    Validates that defined symbol has a corresponding #undef and checks if the
+    symbol is used internally in the override file.
+    """
+    found = {'def_position': -1, 'undef_position': False, 'other': False}
+    pattern = rf'^(.*\b{target}\b.*)$'
+    matches = re.finditer(pattern, content, re.MULTILINE)
+    for match in matches:
+        line = match.group()
+        if re.match(rf'^#define[\s\\]+{target}', line):
+            # Found #define of the target
+            found['def_position'] = match.start()
+        elif (re.match(rf'^#undef[\s\\]+{target}', line) and
+               found['def_position'] >= 0 and
+                 match.start() > found['def_position']):
+            # Found #undef of the target
+            found['undef_position'] = True
+        else:
+            # Found internal use of the target
+            found['other'] = True
+
+    error_count = 0
+    if found['def_position'] == -1:
+        print(f"SCRIPT ERROR: Expected to find #define {target} in " +
+              f"{override_filepath}.")
+        print("-------------------------")
+        error_count = 1
+    if not found['undef_position']:
+        print(
+            f"ERROR: Expected to find #undef {target} in {override_filepath}.")
+        if override_filepath.endswith('.h'):
+            print("       If this symbol is intended to propagate beyond this" +
+                  " header then add it to exceptions in this script.")
+        print("-------------------------")
+        error_count += 1
+    return (error_count, found['other'])
+
+
+def is_header_guard_define(override_filepath, target):
+    """
+    Checks if the target define is the header guard for the override file path
+    """
+    guard_string = ("BRAVE_CHROMIUM_SRC_" +
+                    override_filepath.
+                    replace('\\', '/').
+                    replace('/', '_').
+                    replace('-', '_').
+                    replace('.', '_').
+                    upper() + "_")
+    return target == guard_string
 
 
 def do_check_defines(override_filepath, original_filepath):
@@ -167,30 +212,37 @@ def do_check_defines(override_filepath, original_filepath):
     Finds `#define TARGET REPLACEMENT` statements in |override_filepath| and
     attempts to find the <TARGET> in the |original_filepath|.
     """
+    error_count = 0
     with open(override_filepath, mode='r', encoding='utf-8') as override_file:
-        content = override_file.read()
+        content = strip_comments(override_file.read())
         matches = []
 
         # Search for all matches for normal definitions
         # e.g. #define FooBar FooBar_ChromiumImpl
-        normal_matches = re.findall(NORMAL_DEFINITIONS_REGEXP, content)
+        normal_matches = re.findall(NORMAL_DEFINITIONS_REGEXP, content,
+                                    flags=re.MULTILINE)
         matches += [{'value': m, 'is_func': False} for m in normal_matches]
 
         # Search for all matches for function-like definitions
         # e.g. #define FooBar(P1, P2) FooBar_ChromiumImpl(P1, P2, p3)
         function_matches = re.findall(FUNCTION_LIKE_DEFINITIONS_REGEXP,
-                                      content, flags=re.DOTALL)
+                                      content, flags=re.MULTILINE)
         matches += [{'value': m, 'is_func': True} for m in function_matches]
 
         if not matches:
-            return
+            return error_count
 
         for match in matches:
             target = match['value'][0]
-            replacement = match['value'][1]
 
-            if not replacement:
+            # Skip header guard defines
+            if (override_filepath.endswith(".h") and
+                 is_header_guard_define(override_filepath, target)):
                 continue
+
+            err_count, used_internally = validate_define(
+                override_filepath, content, target)
+            error_count += err_count
 
             # Adjust target name for BUILDFLAG_INTERNAL_*() cases for
             # function-like matches.
@@ -202,20 +254,31 @@ def do_check_defines(override_filepath, original_filepath):
             # Report ERROR if target can't be found in the original file.
             with open(original_filepath, mode='r', encoding='utf-8') as \
                 original_file:
-                if target not in original_file.read():
-                    print(f"ERROR: Unable to find symbol {target}" +
-                          f" in {original_filepath}")
-                    print("-------------------------")
+                if target not in strip_comments(original_file.read()):
+                    if not used_internally:
+                        print(f"ERROR: Unable to find symbol {target}" +
+                              f" in {original_filepath}")
+                        print("-------------------------")
+                        error_count += 1
+                    else:
+                        print(f"INFO: Ignoring symbol {target}:")
+                        print("      Symbol is used internally in " +
+                              "chromium_src/" +
+                              override_filepath.replace('\\', '/'))
+                        print(f"      Symbol is NOT found in " +
+                              f"{original_filepath}.")
+                        print("-------------------------")
+    return error_count
 
 
 def do_check_overrides(overrides_list,
                        search_dir,
-                       check_includes=False,
                        gen_dir=None):
     """
     Checks that each path in the passed in list |overrides_list| exists in
     the passed in directory (|search_dir|), optionally checking includes too.
     """
+    error_count = 0
     print("--------------------------------------------------")
     print(f"Checking overrides in {search_dir} ...")
     print("--------------------------------------------------")
@@ -231,13 +294,20 @@ def do_check_overrides(overrides_list,
         else:
             original_filepath_found = True
         if not original_filepath_found:
-            print(f"WARNING: No source for override {override_filepath}")
+            print(f"ERROR: No source for override {override_filepath}")
+            print("       If this is not a true override, then add the path" +
+                  " to the PATH_EXCLUDES in this script.")
+            print("       Otherwise, the upstream file is gone and a fix is" +
+                  " required.")
             print("-------------------------")
+            error_count += 1
             continue
 
-        do_check_defines(override_filepath, original_filepath)
-        if check_includes:
-            do_check_includes(override_filepath)
+        error_count += do_check_defines(override_filepath, original_filepath)
+        error_count += do_check_includes(override_filepath)
+    if not error_count:
+        print("OK.")
+    return error_count
 
 
 def get_generated_builddir(build, target_os=None, target_arch=None):
@@ -292,7 +362,7 @@ def validate_exclusions(paths):
     Otherwise the developer who removed the excluded file should also remove
     it from the exclusions list.
     """
-    result = True
+    error_count = 0
     print("--------------------------------------------------")
     print("Validating exclusions...")
     print("--------------------------------------------------")
@@ -304,9 +374,10 @@ def validate_exclusions(paths):
             print("       If the file was removed then also remove it from" +
                   " the exclusions list of this script.")
             print("-------------------------")
-            result = False
-    if result: print("OK.")
-    return result
+            error_count += 1
+    if not error_count:
+        print("OK.")
+    return error_count
 
 
 def main(args):
@@ -334,7 +405,7 @@ def main(args):
             return 1
 
     # Check non-RE exclusions
-    ok = validate_exclusions(PATH_EXCLUDES)
+    error_count = validate_exclusions(PATH_EXCLUDES)
 
     # Change into the chromium_src directory for convenience.
     os.chdir(BRAVE_CHROMIUM_SRC)
@@ -343,14 +414,17 @@ def main(args):
     src_overrides = filter_chromium_src_filepaths(
         exclude_regexp='|'.join(RE_EXCLUDES + PATH_EXCLUDES + GRIT_INCLUDES +
                                 ['python_modules|.*grit.*']))
-    do_check_overrides(src_overrides, CHROMIUM_SRC, True, gen_buildir)
+    error_count += do_check_overrides(src_overrides, CHROMIUM_SRC, gen_buildir)
 
     # Check GRIT overrides.
     grit_overrides = filter_chromium_src_filepaths(
         include_regexp='|'.join(GRIT_INCLUDES + ['.*grit.*']),
         exclude_regexp='|'.join(GRIT_EXCLUDES))
-    do_check_overrides(grit_overrides, gen_buildir, False)
-    return 0 if ok else 1
+    error_count += do_check_overrides(grit_overrides, gen_buildir)
+    if error_count:
+        print("--------------------------------------------------")
+        print(f"Found {error_count} error(s).")
+    return error_count
 
 
 if __name__ == '__main__':
