ccd2a9281b21936e343f2ec744681e7b1037a7b0
Anton Lazarev <antonok35@gmail.com>
2023-09-28 22:10:24 -0700

    remove LoadExtensionAsComponent from AdBlockServiceTest

--- browser/brave_shields/ad_block_service_browsertest.cc
+++ browser/brave_shields/ad_block_service_browsertest.cc
@@ -225,11 +225,8 @@ bool AdBlockServiceTest::InstallDefaultAdBlockComponent(
           "Default lists for Brave Browser", true, true, true, 0,
           kDefaultAdBlockComponentTestId,
           kDefaultAdBlockComponentTest64PublicKey, "", "")});
-  const extensions::Extension* ad_block_component = LoadExtensionAsComponent(
-      test_data_dir.AppendASCII("adblock-data").AppendASCII(component_dir));
-  if (!ad_block_component) {
-    return false;
-  }
+  auto component_path =
+      test_data_dir.AppendASCII("adblock-data").AppendASCII(component_dir);
 
   auto& component_providers = g_brave_browser_process->ad_block_service()
                                   ->component_service_manager()
@@ -237,7 +234,7 @@ bool AdBlockServiceTest::InstallDefaultAdBlockComponent(
   EXPECT_EQ(component_providers.size(), 1UL);
   auto& default_provider = component_providers.at("default");
   EXPECT_TRUE(default_provider);
-  default_provider->OnComponentReady(ad_block_component->path());
+  default_provider->OnComponentReady(component_path);
 
   WaitForAdBlockServiceThreads();
 
@@ -268,12 +265,8 @@ bool AdBlockServiceTest::InstallRegionalAdBlockComponent(
       ->SetFilterListCatalog(filter_list_catalog);
 
   if (enable_list) {
-    const extensions::Extension* ad_block_component =
-        LoadExtensionAsComponent(test_data_dir.AppendASCII("adblock-data")
-                                     .AppendASCII("adblock-regional"));
-    if (!ad_block_component) {
-      return false;
-    }
+    auto component_path = test_data_dir.AppendASCII("adblock-data")
+                              .AppendASCII("adblock-regional");
 
     g_brave_browser_process->ad_block_service()
         ->component_service_manager()
@@ -290,8 +283,7 @@ bool AdBlockServiceTest::InstallRegionalAdBlockComponent(
                                 ->additional_filters_engine_.get();
     EngineTestObserver regional_engine_observer(regional_engine);
     auto regional_filters_provider = regional_filters_providers.find(uuid);
-    regional_filters_provider->second->OnComponentReady(
-        ad_block_component->path());
+    regional_filters_provider->second->OnComponentReady(component_path);
     regional_engine_observer.Wait();
   }
 
