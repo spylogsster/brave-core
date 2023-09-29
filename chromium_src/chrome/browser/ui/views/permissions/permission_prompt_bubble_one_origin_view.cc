d1590124b84425c2331e2c51b97e78cac95a18fe
Emerick Rogul <erogul@brave.com>
2023-09-29 10:19:10 -0400

    Reanchor IDS_PERMISSIONS_BUBBLE_PROMPT_OVERRIDE
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/6d94d30c806b26179d2451021d17056f0c86a6e0
    
    commit 6d94d30c806b26179d2451021d17056f0c86a6e0
    Author: Kamila <hkamila@google.com>
    Date:   Tue Sep 19 07:32:19 2023 +0000
    
        [PEPC] Add view that prompts the user for site level permission
    
        Adds the view that prompts the user for site level permission. This
        view is applicable when the site is in an Ask state.
    
        Bug: 1462930

---
+++ chromium_src/chrome/browser/ui/views/permissions/permission_prompt_bubble_one_origin_view.cc
@@ -0,0 +1,11 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/grit/generated_resources.h"
+
+#undef IDS_PERMISSIONS_BUBBLE_PROMPT
+#define IDS_PERMISSIONS_BUBBLE_PROMPT IDS_BRAVE_PERMISSIONS_BUBBLE_PROMPT
+#include "src/chrome/browser/ui/views/permissions/permission_prompt_bubble_one_origin_view.cc"
+#undef IDS_PERMISSIONS_BUBBLE_PROMPT
